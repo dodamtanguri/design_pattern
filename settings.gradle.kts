e9f82204bdde3a4f3aeca98bf04dd758a67fa422
sohee <sh663746@kpcard.co.kr>
2024-07-11 10:36:50 +0900

    프로젝트 모듈 정리

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,8 @@ plugins {
 rootProject.name = "design_pattern"
 include("src:main:java")
 findProject(":src:main:java")?.name = "java"
+include("src:main:java:untitled")
+findProject(":src:main:java:untitled")?.name = "untitled"
+include("src:main:java")
+findProject(":src:main:java")?.name = "java"
+include("java")
