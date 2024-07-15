80d689209f9fd185154237632dea6edcad845194
sohee <sh663746@kpcard.co.kr>
2024-07-15 17:08:49 +0900

    싱글톤-자바

--- java/src/main/java/singleton/SingletonEnum.java
+++ java/src/main/java/singleton/SingletonEnum.java
@@ -3,26 +3,26 @@ package singleton;
 enum SingletonEnum {
     INSTANCE;
 
-    private final Client dbClient;
+//    private final Client dbClient;
+//
+//    SingletonEnum() {
+//        dbClient = Database.getClient();
+//    }
+//
+//    public static SingletonEnum getInstance() {
+//        return INSTANCE;
+//    }
+//
+//    public Client getClient() {
+//        return dbClient;
+//    }
 
-    SingletonEnum() {
-        dbClient = Database.getClient();
-    }
-
-    public static SingletonEnum getInstance() {
-        return INSTANCE;
-    }
-
-    public Client getClient() {
-        return dbClient;
-    }
-
-    public class Main {
-        public static void main(String[] args) {
-            SingletonEnum singleton = SingletonEnum.getInstance();
-            singleton.getClient();
-        }
-
-
-    }
+//    public class Main {
+//        public static void main(String[] args) {
+//            SingletonEnum singleton = SingletonEnum.getInstance();
+//            singleton.getClient();
+//        }
+//
+//
+//    }
 }
